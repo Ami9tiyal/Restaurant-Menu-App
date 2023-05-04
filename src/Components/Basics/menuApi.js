15013005ff3719578784cdfe9813ff3d5007126c
Amit Nautiyal <amitnautiyal996@gmail.com>
2023-05-04 20:17:40 +0530

    First commit

---
+++ src/Components/Basics/menuApi.js
@@ -0,0 +1,85 @@
+const Menu = [
+  {
+    id: 1,
+    image: "images/maggi.jpg",
+    name: "maggi",
+    category: "breakfast",
+    price: "12₹",
+    description: " This dish consists of perfectly cooked noodles that are tossed in a flavorful blend of spices, herbs, and vegetables. It's the perfect comfort food that will warm your soul and leave you feeling satisfied.",
+  },
+
+  {
+    id: 2,
+    image: "images/allupakoida.jpg",
+    name: "aaloo pakoda",
+    category: "evening",
+    price: "20₹",
+    description:
+      "Crispy potato fritters with Indian spices, a popular vegetarian snack, served with chutney or sauce.",
+  },
+  {
+    id: 3,
+    image: "images/corn.jpg",
+    name: "corn",
+    category: "breakfast",
+    price: "10₹",
+    description:
+      "Grilled corn on the cob, seasoned with butter, salt, and pepper, a classic summer side dish.",
+  },
+  {
+    id: 4,
+    image: "images/chola.jpg",
+    name: "chola",
+    category: "lunch",
+    price: "50₹",
+    description:
+      "A spicy North Indian curry made with chickpeas, onions, tomatoes, and aromatic spices, served with rice.",
+  },
+  {
+    id: 5,
+    image: "images/pizza.jpg",
+    name: "pizza",
+    category: "evening",
+    price: "80₹",
+    description:
+      "Italian classic flatbread topped with tomato sauce, cheese, and various toppings, baked to perfection.",
+  },
+  {
+    id: 6,
+    image: "images/nonvegthali.jpg",
+    name: "Non-Veg Thali",
+    category: "dinner",
+    price: "180₹",
+    description:
+      "An Indian platter with assorted meat dishes, lentils, vegetables, bread, rice, and condiments, a complete meal.",
+  },
+  {
+    id: 7,
+    image: "images/sweet.jpg",
+    name: "Sweets",
+    category: "dinner",
+    price: "60₹",
+    description:
+      "Delectable desserts ranging from cakes, pies, and pastries to traditional Indian sweets like gulab jamun.",
+  },
+  {
+    id: 8,
+    image: "images/rajmarice.jpg",
+    name: "Rajma Rice",
+    category: "lunch",
+    price: "60₹",
+    description:
+      "A North Indian delicacy made with red kidney beans, spices, and served with steamed rice.",
+  },
+  {
+    id: 9,
+    image: "images/samosa.jpg",
+    name: "samosa",
+    category: "evening",
+    price: "10₹",
+    description:
+      "Crispy triangular pastry filled with spiced potatoes, peas, or meat, a popular Indian snack.",
+  },
+];
+
+export default Menu;
